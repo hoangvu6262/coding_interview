e8341cb1e5b9c24a8724889a828e19bca69edd55
hoangvu6262 <vucao@namlongsoft.net>
2023-09-13 17:25:48 +0700

    set up httl and axios

---
+++ configs/https/axios.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { BASE_URL } from '@/configs/constants/urlConstant'
+
+export const apiClient = () => {
+    console.log(BASE_URL)
+    const axiosHttp = axios.create({
+        baseURL: BASE_URL,
+    })
+
+    axiosHttp.interceptors.request.use(
+        async function intercept(config) {
+            const interceptedConfig = config
+            return interceptedConfig
+        },
+        function interceptError(error) {
+            return Promise.reject(error)
+        }
+    )
+
+    axiosHttp.interceptors.response.use(function intercept(response) {
+        return response
+    })
+
+    return axiosHttp
+}
